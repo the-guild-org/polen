213ec6c29baea90a34976cc1e52ce69788881cdf
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-30 09:38:44 -0400

    debug

--- src/lib/project-controller/project-controller.ts
+++ src/lib/project-controller/project-controller.ts
@@ -137,6 +137,8 @@ export const create = async <scriptRunners extends ScriptRunners = {}>(
 
   debug(`created temporary directory`, { path: fsj.cwd() })
 
+  console.log(`what am i`)
+  console.log($)
   const shell = $({ cwd: fsj.cwd() })
 
   const pnpmShell = shell({ prefix: `pnpm ` })
