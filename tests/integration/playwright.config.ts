1566e3525d4dad09813693c00cebf0fa6b000b5d
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-30 14:24:16 -0400

    no pw config

--- tests/integration/playwright.config.ts
+++ tests/integration/playwright.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig, devices } from 'playwright/test'
+// import { defineConfig, devices } from 'playwright/test'
 
-const isCi = !!process.env[`CI`]
+// const isCi = !!process.env[`CI`]
 
-export default defineConfig({
-  name: `examples`,
-  testDir: `./cases`,
-  fullyParallel: true,
-  forbidOnly: isCi,
-  retries: 0, // isCi ? 2 : 0,
-  workers: isCi ? 1 : undefined,
-  outputDir: `./__results__`,
-  use: {
-    trace: `on-first-retry`,
-    screenshot: `only-on-failure`,
-  },
-  projects: [
-    {
-      name: `chromium`,
-      use: { ...devices[`Desktop Chrome`] },
-    },
-  ],
-})
+// export default defineConfig({
+//   name: `examples`,
+//   testDir: `./cases`,
+//   fullyParallel: true,
+//   forbidOnly: isCi,
+//   retries: 0, // isCi ? 2 : 0,
+//   workers: isCi ? 1 : undefined,
+//   outputDir: `./__results__`,
+//   use: {
+//     trace: `on-first-retry`,
+//     screenshot: `only-on-failure`,
+//   },
+//   projects: [
+//     {
+//       name: `chromium`,
+//       use: { ...devices[`Desktop Chrome`] },
+//     },
+//   ],
+// })
