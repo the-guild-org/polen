2d14a05efa7c2b83eaa00c05d834ecc0bfb954a0
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-30 11:37:02 -0400

    debug

---
+++ debug.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'playwright/test'
+import { $ } from 'zx'
+
+console.log((await import(`zx`)).$)
+console.log((await import(`zx/core`)).$)
+
+
+test(`is there`, () => {
+  expect($).toBeDefined()
+})
