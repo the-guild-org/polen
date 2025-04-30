010a67b914dac8d9dbbb1926f38dba08b80086f1
Jason Kuhrt <jasonkuhrt@me.com>
2025-04-30 16:25:22 -0400

    vitest fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import ViteTsconfigPaths from'vite-tsconfig-paths'
 
 export default defineConfig({
+  plugins:[ViteTsconfigPaths()],
   test: {
     include: [`src/**/*.test.ts`],
   },
