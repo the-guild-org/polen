beabd2a9840b18ac7e06203a772236b279b0ec95
Jason Kuhrt <jasonkuhrt@me.com>
2025-05-20 21:58:32 -0400

    fix examples tests

--- src/exports/dependencies/react/jsx-runtime.ts
+++ src/exports/dependencies/react/jsx-runtime.ts
@@ -1,4 +1,3 @@
-import { jsx, jsxs } from 'react/jsx-runtime'
+export { Fragment, jsx, jsxs } from 'react/jsx-runtime'
+// Leads to Rollup error because react/jsx-runtime is not real ESM
 // export * from 'react/jsx-runtime'
-
-export { jsx, jsxs }
