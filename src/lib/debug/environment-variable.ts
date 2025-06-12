6f1b37ae1c319f964ea5d003f21da856ddb4e2b9
Jason Kuhrt <jasonkuhrt@me.com>
2025-06-12 15:19:12 -0400

    improve(debug): add support for namespace lists

--- src/lib/debug/environment-variable.ts
+++ src/lib/debug/environment-variable.ts
@@ -2,6 +2,7 @@ import { Arr, Language } from '@wollybeard/kit'
 
 export const enVarName = `DEBUG`
 
+export const deliminator = `,`
 export const wildcard = `*`
 export const enVarEnabledValuesStatic = [`true`, wildcard, `1`]
 
@@ -10,29 +11,45 @@ export const calcIsEnabledFromEnv = (
   namespace?: string[],
 ): boolean => {
   const namespace_ = namespace?.map(_ => _.toLowerCase())
-  const pattern = typeof enVars[enVarName] === `string`
+
+  const includeFilters = typeof enVars[enVarName] === `string`
     ? enVars[enVarName]
       .trim()
       .toLowerCase()
-      .split(`:`)
-      .map(_ => _.trim())
+      .split(deliminator)
+      .map(patternExpression => {
+        return patternExpression
+          .trim()
+          .split(`:`)
+          .map(_ => _.trim())
+      })
     : undefined
 
-  if (!pattern) return false
+  if (!includeFilters) return false
 
-  if (pattern.length === 1 && enVarEnabledValuesStatic.includes(pattern[0]!)) return true
+  if (includeFilters.length === 0) return false
 
+  // If any is like * then it means "enable everything"
+  if (
+    includeFilters.some(includeFilter => {
+      if (includeFilter.length === 1 && enVarEnabledValuesStatic.includes(includeFilter[0]!)) return true
+    })
+  ) return true
+
+  // At this point, if there is no namespace (e.g. root), then we cannot match anything
   if (!namespace_) return false
 
-  if (Arr.getLast(pattern) !== wildcard) {
-    return pattern.join() === namespace_.join()
-  }
+  for (const includeFilter of includeFilters) {
+    if (Arr.getLast(includeFilter) !== wildcard) {
+      return includeFilter.join() === namespace_.join()
+    }
 
-  let i = 0
-  for (const segment of pattern) {
-    if (segment === wildcard) return true
-    if (segment !== namespace_[i]) return false
-    i++
+    let i = 0
+    for (const segment of includeFilter) {
+      if (segment === wildcard) return true
+      if (segment !== namespace_[i]) return false
+      i++
+    }
   }
 
   Language.never()
