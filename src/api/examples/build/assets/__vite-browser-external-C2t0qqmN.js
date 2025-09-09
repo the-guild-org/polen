787fbd3fe9f92d166d7799cc031b93a8a882a371
Jason Kuhrt <jasonkuhrt@me.com>
2025-09-09 09:49:34 +0200

    fix: remove build!!

--- src/api/examples/build/assets/__vite-browser-external-C2t0qqmN.js
+++
@@ -1,5 +0,0 @@
-import { b as e } from './entry.client-DU2Tx777.js'
-var t = e((exports, t) => {
-  t.exports = {}
-})
-export default t()
--- src/api/examples/build/assets/ts-tags-CWTc7CEI.js
+++
@@ -1,6 +0,0 @@
-var e = `TypeScript with Tags`,
-  t = `ts-tags`,
-  n = [{ include: `source.ts` }],
-  r = `source.ts.tags`,
-  i = { displayName: e, name: t, patterns: n, scopeName: r }
-export { e as displayName, i as default, n as patterns, r as scopeName, t as name }
